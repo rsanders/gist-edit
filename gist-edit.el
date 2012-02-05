405d2832bd280782ce060bef58fa84db58e50604
Robert Sanders <robert@zeevex.com>
2012-02-05 12:21:13 -0500

    added some keybindings to visit gist web page, list all owned gists

--- gist-edit.el
+++ gist-edit.el
@@ -1,7 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 
-;; (require 'magit)
-;; (require 'magithub)
+;; (add-hook 'after-init-hook
+;;           (lambda ()
+;;             (require 'magit)
+;;             (require 'magithub)))
+
+(require 'magit)
+(require 'magithub)
+(autoload 'browse-url-generic "browse-url")
+(autoload 'gist               "gist")
+(autoload 'gist               "gist-list")
 
 (defgroup gist-edit nil
   "Customization for the 'gist-edit' package for editing GitHub gists (git-based pasties)"
@@ -50,37 +58,26 @@
    (equal 0
           (string-match gist-edit/tmp-directory dir))))
 
-(defun gist-edit/list ()
-  "Opens a list of open gists"
-  (interactive)
-  (find-file gist-edit/tmp-directory))
-
 (defun gist-edit/open (dir)
   "Opens an already checked out gist for editing"
- (find-file dir)
+  (find-file dir)
   (magit-status dir))
 
 (defun gist-edit/setup-directory (number repo dir &optional variables)
   (with-temp-buffer
     (let ((alist
-           `((nil . ((gist-edit-repo . ,repo)
+           `((nil . ((gist-edit-repo   . ,repo)
                      (gist-edit-number . ,number)
                      (gist-edit-buffer . t)
                      ,@variables
                      (eval . (gist-edit-mode t)))))))
-      (princ alist (current-buffer))
+      (print alist (current-buffer))
       (write-file (concat dir "/.dir-locals.el"))
       (append-to-file ".dir-locals.el\n" nil (concat dir "/.git/info/exclude"))
       )
     )
   )
 
-(defun gist-edit (gistnum)
-  (interactive "MGist Number: ")
-  (let ((dir (gist-edit/local-gist-dir gistnum)))
-    (unless (file-exists-p dir)
-      (gist-edit/clone (gist-edit/gist-url gistnum) dir))
-    (gist-edit/open dir)))
 
 (defun gist-edit/clone (repo destdir)
   "Clone an existing gist into a local directory"
@@ -91,13 +88,49 @@
     (gist-edit/setup-directory number repo dir)
     (gist-edit/open dir)))
 
+(defun gist-edit/web-url ()
+  "The URL for the Gist web page"
+  (format "https://gist.github.com/%s" gist-edit-number))
+
+
+
+(defun gist-edit (gistnum)
+  (interactive "MGist Number: ")
+  (let ((dir (gist-edit/local-gist-dir gistnum)))
+    (unless (file-exists-p dir)
+      (gist-edit/clone (gist-edit/gist-url gistnum) dir))
+    (gist-edit/open dir)))
+
+(defun gist-edit/browse-web-url ()
+  "Open the URL for the Gist web page in the system browser"
+  (interactive)
+  (browse-url-generic (gist-edit/web-url)))
+
+(defun gist-edit/finish ()
+  "Save current file, commit all changes, and push"
+  (interactive)
+  (save-buffer)
+  (unless (magit-everything-clean-p)
+    (magit-stage-all)
+    (magit-run-git "commit" "-m" "automatically updated gist"))
+  (magit-run-git "push")
+  (message "Gist updated!"))
+
+(defun gist-edit/list ()
+  "Opens a list of open gists"
+  (interactive)
+  (find-file gist-edit/tmp-directory))
+
 (defvar gist-edit/keymap
   (let ((map (make-sparse-keymap)))
     (define-key map (char-to-string help-char) 'gist-edit/help)
     (define-key map [help] 'gist-edit/help)
     (define-key map [f1] 'gist-edit/help)
-    (define-key map "p" 'gist-edit/push)
+    (define-key map "b" 'gist-edit/browse-web-url)
     (define-key map "f" 'gist-edit/finish)
+    (define-key map "l" 'gist-edit/list)
+    (define-key map "o" 'gist-edit)
+    (define-key map "p" 'gist-edit/push)
     map)
   "Keymap for Gist Edit mode.")
 
